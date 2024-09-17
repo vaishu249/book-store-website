129d73c87ba5d141d6378c925f0db31643503a65
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-17 19:18:35 +0530

    Ading slider

--- Frontend/src/Components/Freebook.jsx
+++ Frontend/src/Components/Freebook.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
+// slider for cards 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import list from "../../public/list.json"
 const Freebook = () => {
+  //filter out the data with a category 
+  const filterData=list.filter((data) =>data.category==="Free");
   return (
-    <div>
-      hello
+    <>
+    <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
+      <h1 className='font-semibold text-xl pd-2'>
+        <span className='text-orange-500 text-3xl'>Free books</span> Offered for You !!!
+      </h1>
+      <p>Discover a world of knowledge and imagination with
+         our exclusive free book offer! For a limited time, 
+         we're giving away captivating reads from a wide variety 
+         of genres, including fantasy, mystery, romance, self-help, 
+         and more.
+      </p>
     </div>
+    </>
   )
 }
 
