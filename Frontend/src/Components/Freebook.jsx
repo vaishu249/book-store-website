f09f7add30a652f2a4d33ea56585f4ada865beb5
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-17 19:31:31 +0530

    Created a card

--- Frontend/src/Components/Freebook.jsx
+++ Frontend/src/Components/Freebook.jsx
@@ -4,23 +4,90 @@ import React from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import Slider from "react-slick";
+
 import list from "../../public/list.json"
 const Freebook = () => {
   //filter out the data with a category 
-  const filterData=list.filter((data) =>data.category==="Free");
+  const filterData = list.filter((data) => data.category === "Free");
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <>
-    <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
-      <h1 className='font-semibold text-xl pd-2'>
-        <span className='text-orange-500 text-3xl'>Free books</span> Offered for You !!!
-      </h1>
-      <p>Discover a world of knowledge and imagination with
-         our exclusive free book offer! For a limited time, 
-         we're giving away captivating reads from a wide variety 
-         of genres, including fantasy, mystery, romance, self-help, 
-         and more.
-      </p>
-    </div>
+      <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
+        <div>
+          <h1 className='font-semibold text-xl pd-2'>
+            <span className='text-orange-500 text-3xl'>Free books</span> Offered for You !!!
+          </h1>
+          <p>Discover a world of knowledge and imagination with
+            our exclusive free book offer! For a limited time,
+            we're giving away captivating reads from a wide variety
+            of genres, including fantasy, mystery, romance, self-help,
+            and more.
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
     </>
   )
 }
