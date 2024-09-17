4ba5b4092f87cfdbfe7edbd7602a0d9625fb8ba5
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-17 14:25:11 +0530

    Created file for footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Banner from './Components/Banner'
+import Footer from './Components/Footer'
 const App = () => {
   return (
     <div >
       <Navbar/>
       <Banner/>
+      <Footer/>
     </div>
   )
 }
