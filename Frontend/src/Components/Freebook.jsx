feb591caf12533ef0c094492dc359b65141837e6
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-17 20:47:48 +0530

    small changes

--- Frontend/src/Components/Freebook.jsx
+++ Frontend/src/Components/Freebook.jsx
@@ -47,12 +47,12 @@ const Freebook = () => {
   };
   return (
     <>
-      <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
+      <div className='max-w-screen-2x1 container mx-auto md:px-20 px-5'>
         <div>
-          <h1 className='font-semibold text-xl pd-2'>
-            <span className='text-orange-500 text-3xl'>Free books</span> Offered for You !!!
+          <h1 className='font-semibold text-xl'>
+            <span className='text-orange-500 text-2xl '>Free books</span> Offered for You !!!
           </h1>
-          <p>Discover a world of knowledge and imagination with
+          <p className='pt-4'>Discover a world of knowledge and imagination with
             our exclusive free book offer! For a limited time,
             we're giving away captivating reads from a wide variety
             of genres, including fantasy, mystery, romance, self-help,
