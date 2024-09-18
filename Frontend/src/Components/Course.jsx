3ad808b757c9e26ba0eed3c396a24b472bb7a32d
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-18 21:49:03 +0530

    Added heading and paragraph

--- Frontend/src/Components/Course.jsx
+++ Frontend/src/Components/Course.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 
 const Course = () => {
-  return (
-    <>
-        <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
-            <div className='mt-28 item-center justify-center text-center'>
-                <h1 className='text-2xl md:text-4xl'>vaishnavi</h1>
-                <p></p>
+    return (
+        <>
+            <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
+                <div className='mt-28 item-center justify-center text-center'>
+                    <h1 className='text-2xl md:text-4xl'>
+                        Master the Art of Reading with Our <span className='text-orange-500'>Exclusive Book</span> Collection
+                    </h1>
+                    <p className='mt-10'>
+                        Welcome to the ultimate destination for book lovers!
+                        Whether you're a seasoned reader or just beginning your
+                        literary journey, our bookstore offers a carefully curated
+                        selection of books across all genres. From timeless classics
+                        to the latest bestsellers, we are passionate about bringing
+                        the world of knowledge, imagination, and inspiration to your
+                        fingertips.
+                    </p>
+                    <button className='bg-orange-500 text-white px-5 py-2 rounded-md hover:bg-slate-900 duration-300 cursor-pointer mt-6'>
+                        Back
+                    </button>
+                </div>
+                <div>
+                    
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Course
