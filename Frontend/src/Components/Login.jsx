1ca39787e45b4aa0319d69135b500ba4b7b9ce8a
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-19 23:33:44 +0530

    Created a login page

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -9,8 +9,19 @@ const Login = () => {
                         {/* if there is a button in form, it will close the modal */}
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                    <h3 className="font-bold text-lg">Login</h3>
+                    <div className='mt-4 space-y-2'>
+                        <span>Email</span><br />
+                        <input type="email" placeholder='Enter Your Email' className='md:w-96 w-80 py-1 px-3 border rounded-md outline-none'/>
+                    </div>
+                    <div className='mt-4 space-y-2'>
+                        <span>Email</span><br />
+                        <input type="password" placeholder='Enter Your Password' className='md:w-96 w-80 py-1 px-3 border rounded-md outline-none'/>
+                    </div>
+                    <div className='flex justify-around mt-4'>
+                        <button className='bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-slate-900 duration-300 cursor-pointer'>Login</button>
+                        <p>Not Registered <span className='underline text-blue-500 cursor-pointer'>Signup</span></p>
+                    </div>
                 </div>
             </dialog>
         </div>
